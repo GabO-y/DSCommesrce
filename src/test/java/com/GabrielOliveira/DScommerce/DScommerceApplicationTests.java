a6c0caa8bad8c10569280a2ddd8172f1d23124cc
GabO-y <gabrielosilva1000@gmail.com>
2025-02-06 12:24:30 -0300

    first commit

---
+++ src/test/java/com/GabrielOliveira/DScommerce/DScommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielOliveira.DScommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DScommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
