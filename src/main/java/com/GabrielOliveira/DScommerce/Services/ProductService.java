a6f1c18b7ecbfcbd952ce13415eb924828571c8e
GabO-y <gabrielosilva1000@gmail.com>
2025-02-08 11:13:52 -0300

    get product pages

--- src/main/java/com/GabrielOliveira/DScommerce/Services/ProductService.java
+++ src/main/java/com/GabrielOliveira/DScommerce/Services/ProductService.java
@@ -2,10 +2,12 @@ package com.GabrielOliveira.DScommerce.Services;
 
 import com.GabrielOliveira.DScommerce.Repository.ProductRepository;
 import com.GabrielOliveira.DScommerce.dto.ProductDTO;
-import com.GabrielOliveira.DScommerce.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,6 +19,11 @@ public class ProductService {
     public ProductDTO findById(Long id){
 
         return new ProductDTO(repository.findById(id).orElseThrow());
+    }
+
+
+    public Page<ProductDTO> findAll(Pageable page){
+        return repository.findAll(page).map(x -> new ProductDTO(x));
     }
 
 }
